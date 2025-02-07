791678c05d19aa82125e3152908d3573bb47e2c3
Callen Mica <callenmicah@gmail.com>
2025-02-07 15:03:26 +0800

    Auth & DB Sync

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-function Navbar() {
+async function Navbar() {
+  const user = await currentUser();
+  if (user) await syncUser(); // POST REQ
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
@@ -12,7 +16,7 @@ function Navbar() {
               href="/"
               className="text-xl font-bold text-primary font-mono tracking-wider"
             >
-              Socially
+              Papus Social
             </Link>
           </div>
 
